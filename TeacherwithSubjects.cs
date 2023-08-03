c2c71f4ccd704c621739ca37e5549366df98b828
vivekkumarkar@gmail.com <vivekkumarkar@gmail.com>
2023-08-03 22:37:33 +0530

    first commit

---
+++ TeacherwithSubjects.cs
@@ -0,0 +1,21 @@
+﻿using Practice_Project_4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Project_4
+{
+    public class TeacherwithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherwithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+
+}
